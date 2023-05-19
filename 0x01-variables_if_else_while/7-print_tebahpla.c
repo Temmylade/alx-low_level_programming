b38859fdadc73f098c37d10c01d62d857a44ef81
Temmylade <temiladesijuade@gmail.com>
2023-05-19 13:23:11 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+}
+
