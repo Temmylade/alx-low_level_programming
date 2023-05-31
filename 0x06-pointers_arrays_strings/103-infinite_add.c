0682e32818ea8b0de9934b157d870aaf3b12add3
Temmylade <temiladesijuade@gmail.com>
2023-05-31 13:57:27 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,7 +10,8 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, carry, sum, i, j;
+	int len1, len2, len_sum, carry, sum;
+	int i, j, k;
 
 	len1 = _strlen(n1);
 	len2 = _strlen(n2);
@@ -18,40 +19,52 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (len1 + 1 > size_r || len2 + 1 > size_r)
 		return (0);
 
+	len_sum = (len1 > len2 ? len1 : len2) + 1;
+
+	r[len_sum] = '\0';
+
 	carry = 0;
 	i = len1 - 1;
 	j = len2 - 1;
-
-	r[size_r - 1] = '\0';
-	size_r--;
+	k = len_sum - 1;
 
 	while (i >= 0 || j >= 0)
 	{
 		sum = carry;
 
 		if (i >= 0)
-			sum += n1[i] - '0';
+			sum += n1[i--] - '0';
 
 		if (j >= 0)
-			sum += n2[j] - '0';
+			sum += n2[j--] - '0';
 
-		r[size_r - 1] = sum % 10 + '0';
+		r[k--] = sum % 10 + '0';
 		carry = sum / 10;
-
-		i--;
-		j--;
-		size_r--;
 	}
 
 	if (carry > 0)
-	{
-		if (size_r == 0)
-			return (0);
+		r[k] = carry + '0';
+	else
+		k++;
 
-		r[size_r - 1] = carry + '0';
-	}
-	else if (size_r == 0)
+	if (len_sum - k > size_r)
 		return (0);
 
-	return (r + size_r);
+	return (&r[k]);
+}
+
+/**
+ * _strlen - Calculates the length of a string
+ * @str: The string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
 }
