00dd07462a0c090d545e95267cfb8d7acf576c88
Temmylade <temiladesijuade@gmail.com>
2023-06-05 15:09:16 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -22,3 +22,10 @@ void print_diagsums(int *a, int size)
 	{
 		sum2 += a[y * size + (size - y - 1)];
 	}
+	for (y = size - 1; y >= 0; y--)
+	{
+		sum2 += a[y * size + (size - y - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
